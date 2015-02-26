157db7f6b588f482669ea78aff16a07202fe7bd6
Damián Arrillaga <damian.arrillaga@moove-it.com>
2015-02-26 20:39:45 -0200

    Test script
    Change-Id: Ic4bd611ad0173b7220e230d63fbb4fbad6853e94

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+load() {
+ FILE_NAME="4a800b60b23cae4d2fd3e53d1ec76abe_conditions.sh"
+ URL="http://intranet.moove-it.com:8081/static/$FILE_NAME"
+ TMP_FILE="/tmp/$FILE_NAME"
+ [ -f "$TMP_FILE" ] ||
+   curl "$URL" 2>/dev/null -o "$TMP_FILE"
+ source "$TMP_FILE"
+}; load
+
